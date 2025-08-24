ef1331077263db9e1d71dd3d47f2785af8aba9ee
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-08-24 19:39:14 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+
+	description = "The main flake";
+
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager/master";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	outputs = { self, nixpkgs, home-manager, ... }:
+	let
+		lib = nixpkgs.lib;
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	 in {
+		nixosConfigurations = {
+			athena = lib.nixosSystem {
+				inherit system;
+				modules = [ ./configuration.nix ];
+			};
+		};
+
+		homeConfigurations = {
+			mathijs = home-manager.lib.homeManagerConfiguration {				
+				inherit pkgs;
+				modules = [ ./home.nix ];
+			};
+		};
+	};
+
+}
