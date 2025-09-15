6d52903b504dc9caa66c4a534902890def5ca7fe
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-09-15 07:28:56 +0200

    Added aliases

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -1,19 +1,21 @@
-{ pkgs, lib, ...}:
+{pkgs, lib, ...}:
 
 {
         vim = {
+                viAlias = true;
+                vimAlias = true;
                 theme = {
                         enable = true;
                         name = "gruvbox";
                         style = "dark";
                 };
-
                 statusline.lualine.enable = true;
                 telescope.enable = true;
                 autocomplete.nvim-cmp.enable = true;
                 lsp.enable = true;
                 languages = {
                         enableTreesitter = true;
+                        enableFormat = true;
 
                         nix.enable = true;
                         python.enable = true;
