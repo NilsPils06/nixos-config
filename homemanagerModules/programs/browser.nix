06abe4174b29bf7befeb855dbe91e3bfb76ffa9f
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-09-25 22:14:45 +0200

    Updated zen-browser icon

--- homemanagerModules/programs/browser.nix
+++ homemanagerModules/programs/browser.nix
@@ -6,7 +6,33 @@
 
         config = lib.mkIf config.browser.enable {
                 home.packages = with zen-browser.packages."x86_64-linux"; [ beta ];
-                #programs.zen-browser.enable = true;
+                xdg = {
+                        enable = true;
+                        desktopEntries = {
+                                "zen-beta" = {
+                                        name = "Zen Browser";
+                                        genericName = "Web Browser";
+                                        comment = "A web browser based on Zen";
+                                        exec = "zen-beta %U";
+                                        icon = "zen-browser";
+                                        terminal = false;
+                                        type = "Application";
+                                        mimeType = [
+                                                "text/html"
+                                                "text/xml"
+                                                "application/xhtml+xml"
+                                                "application/vnd.mozilla.xul+xml"
+                                                "x-scheme-handler/http"
+                                                "x-scheme-handler/https"
+                                        ];
+                                        categories = [
+                                                "Network"
+                                                "WebBrowser"
+                                        ];
+                                        startupNotify = true;
+                                };
+                        };
+                };
         };
 
 }
