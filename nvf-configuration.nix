72c14e7561eb6a54a6cc4e066fb9ac7ceac3620e
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-10-01 11:50:16 +0200

    Added git and lualine

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -1,18 +1,27 @@
-{pkgs, lib, ...}:
-
+{...}:
 {
         vim = {
                 viAlias = true;
                 vimAlias = true;
+
                 theme = {
                         enable = true;
                         name = "gruvbox";
                         style = "dark";
                 };
-                statusline.lualine.enable = true;
+
+                statusline.lualine = {
+                        enable = true;
+                };
+
                 telescope.enable = true;
+
                 autocomplete.nvim-cmp.enable = true;
+
                 lsp.enable = true;
+
+                git.enable = true;
+
                 languages = {
                         enableTreesitter = true;
                         enableFormat = true;
