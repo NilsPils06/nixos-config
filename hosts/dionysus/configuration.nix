e5e7a23732a49433d60d211350aa69a9637d040e
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-10-20 16:45:15 +0200

    Switched kernels from lts to zen

--- hosts/dionysus/configuration.nix
+++ hosts/dionysus/configuration.nix
@@ -51,6 +51,7 @@
         ];
 
         boot = {
+                kernelPackages = pkgs.linuxPackages_zen;
                 loader = {
                         systemd-boot.enable = true;
                         efi.canTouchEfiVariables = true;
