9566af23112d43f08a795911016a6e53c06eddde
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2025-10-22 22:29:18 +0200

    Unpinned gramps

--- modules/home-manager/programs/gramps.nix
+++ modules/home-manager/programs/gramps.nix
@@ -5,29 +5,8 @@
         };
 
         config = lib.mkIf config.gramps.enable {
-                home.packages = [
-                        (pkgs.gramps.overrideAttrs (oldAttrs: {
-                                src = pkgs.fetchFromGitHub {
-                                        owner = "gramps-project";
-                                        repo = "gramps";
-                                        rev = "v6.0.4";
-                                        hash = "sha256-MBsc4YMbCvzRG6+7/cGQpx7iYvQAdqWYrIMEpf1A7ew=";
-                                };
-                                version = "6.0.4";
-
-                                propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
-                                        pkgs.python3Packages.orjson
-                                ];
-
-                                patches = builtins.filter (p:
-                                        let
-                                                patchName = builtins.toString p;
-                                                suffix = "disable-gtk-warning-dialog.patch";
-                                        in
-                                                builtins.substring (builtins.stringLength patchName - builtins.stringLength suffix) (builtins.stringLength suffix) patchName != suffix
-                                ) oldAttrs.patches;
-                        }))
-
+                home.packages = with pkgs; [
+                        gramps
                 ];
         };
 }
