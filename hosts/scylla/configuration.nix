5eef2dd160e6631397d832a7e07d4931ef01ecb8
NilsPils06 <135322818+NilsPils06@users.noreply.github.com>
2026-02-12 10:19:19 +0100

    add vbox to scylla

--- hosts/scylla/configuration.nix
+++ hosts/scylla/configuration.nix
@@ -101,4 +101,7 @@
       		    GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
       		  '';
   };
+  virtualisation.virtualbox.host.enable = true;
+   virtualisation.virtualbox.host.enableExtensionPack = true;
+   users.extraGroups.vboxusers.members = [ "nils" ];
 }
