ed69f2e36751d70ac227f1b7646139bc39b03bae
NilsPils06 <135322818+NilsPils06@users.noreply.github.com>
2026-03-23 19:39:13 +0100

    update postgres and temp disable rstudio

--- modules/hosts/kotoamatsukami/default.nix
+++ modules/hosts/kotoamatsukami/default.nix
@@ -41,17 +41,31 @@ in
 
             postgresql = {
               enable = true;
-              ensureDatabases = [ "mydatabase, matchup, matchup_test" ];
+              ensureDatabases = [
+                "mydatabase"
+                "matchup"
+                "matchup_test"
+              ];
+
+              ensureUsers = [
+                {
+                  name = "app";
+                }
+              ];
+
               enableTCPIP = true;
               authentication = pkgs.lib.mkOverride 10 ''
                 local all      all     trust
                 host  all      all     127.0.0.1/32   trust
                 host  all      all     ::1/128        trust
               '';
+
               initialScript = pkgs.writeText "backend-initScript" ''
                 CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
                 CREATE DATABASE nixcloud;
                 GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+                ALTER DATABASE matchup OWNER TO app;
+                ALTER DATABASE matchup_test OWNER TO app;
               '';
             };
           };
